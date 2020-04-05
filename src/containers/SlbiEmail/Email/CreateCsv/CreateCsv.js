bc195cca59ce77c83c564ccee5bc667b14ae1f7f
najathi <najathi777@gmail.com>
2020-04-05 23:13:11 +0530

    starting components

--- src/containers/SlbiEmail/Email/CreateCsv/CreateCsv.js
+++ src/containers/SlbiEmail/Email/CreateCsv/CreateCsv.js
@@ -3,15 +3,55 @@ import { MDBCol, MDBBtn, MDBInputGroup } from 'mdbreact';
 
 const CreateCSV = props => {
 
-	const [username, setUsername] = useState('');
-	const [designation, setDesignation] = useState('');
+	// const [username, setUsername] = useState('');
+	// const [designation, setDesignation] = useState('');
+
+	const [orderForm, setOrderForm] = useState({
+		email: {
+			elementType: 'input',
+			elementConfig: {
+				type: 'email',
+				placeholder: 'Your E-Mail'
+			},
+			value: '',
+			validation: {
+				required: true
+			},
+			valid: false,
+			touched: false
+		},
+		designation: {
+			elementType: 'select',
+			elementConfig: {
+				options: [
+					{ value: 'fastest', displayValue: 'Fastest' },
+					{ value: 'cheapest', displayValue: 'Cheapest' }
+				]
+			},
+			value: 'fastest', // default Value
+			validation: {},
+			valid: true
+		},
+		formIsValid: false,
+		loading: false
+	});
+	const [formIsValid, setFormIsValid] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const formElementArray = [];
+	for (let key in orderForm) {
+		formElementArray.push({
+			id: key,
+			config: orderForm[key]
+		});
+	}
 
 	return (
 		<MDBCol lg="12">
 			<MDBCol md="6">
 				<div className="createForm" style={{ padding: '2rem', textAlign: 'left' }}>
 					<form>
-						<p className="h4 text-left mb-4" style={{ marginBottom: '4rem' }}>Create Bulk Email Accounts</p>
+						{/* <p className="h4 text-left mb-4" style={{ marginBottom: '4rem' }}>Create Bulk Email Accounts</p>
 						<label className="grey-text">
 							Username of Email Address
         		</label>
@@ -29,9 +69,13 @@ const CreateCSV = props => {
 							<option value=".am">Area Manager</option>
 							<option value=".asm">Associate Manager</option>
 							<option value=".sr">Seller / Product Introducer</option>
-						</select>
-						<div className="mt-4">
-							<MDBBtn color="indigo" onClick={() => props.addedEmail(username + designation + '@slbi.lk')}>Add Email</MDBBtn>
+						</select> */}
+
+						{}
+
+						<div style={{ marginTop: '1rem' }}>
+							{/* <MDBBtn type="button" color="indigo" onClick={props.addedEmail.bind(this, username + designation + '@slbi.lk')}>Add Email</MDBBtn> */}
+							<MDBBtn type="button" color="indigo">Add Email</MDBBtn>
 						</div>
 					</form>
 				</div>
