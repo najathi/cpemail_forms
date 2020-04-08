e39dbc66988d1d57eea93e09f220dc07b200ae5d
najathi <najathi777@gmail.com>
2020-04-08 14:26:54 +0530

    updated docs

--- src/components/Docs/Docs.js
+++ src/components/Docs/Docs.js
@@ -11,13 +11,13 @@ const Docs = (props) => {
 			<div className="doc-wrapper" style={{ marginTop: '3rem' }}>
 				<div className="container">
 					<div id="doc-header" className="doc-header text-center">
-						<h1 className="doc-title"><i className="icon fa fa-paper-plane"></i> SLBI Email Documentation</h1>
+						<h3 className="doc-title"><i className="icon fa fa-paper-plane"></i> SLBI Email Documentation</h3>
 					</div>
 					<div className="doc-body row" style={{ marginTop: '3rem', marginBottom: '5rem' }}>
 						<div className="doc-content col-md-12 col-12 order-1">
 							<div className="content-inner">
 								<section id="download-section" className="doc-section">
-									<h2 className="section-title">How to use and create an email / bulk accounts ?</h2>
+									<h3 className="section-title">How to use and create an email / bulk accounts ?</h3>
 									<div className="section-block">
 										<p>This system has two features. such as,</p>
 										<ul>
@@ -26,14 +26,14 @@ const Docs = (props) => {
 										</ul>
 									</div>
 								</section>
-								<section id="download-section" className="doc-section">
-									<h4 className="section-title">Create an Email Account</h4>
+								<section id="download-section" className="doc-section" style={{ marginTop: '2rem' }}>
+									<h5 className="section-title">Create an Email Account</h5>
 									<div className="section-block">
 										<p>This feature, you can add an email account one by one</p>
 									</div>
 								</section>
-								<section id="download-section" className="doc-section">
-									<h4 className="section-title">Create an Email Account</h4>
+								<section id="download-section" className="doc-section" style={{ marginTop: '2rem' }}>
+									<h5 className="section-title">Create Bulk Email Accounts</h5>
 									<div className="section-block">
 										<p>This feature, you can add bulk email accounts. when you add email account that will create a <b>csv</b> file. After Please send the file on whatsapp to Mr. Nahaar. Nahaar will create email accounts for you.</p>
 									</div>
